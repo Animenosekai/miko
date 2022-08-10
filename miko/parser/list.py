9d0190a0721f46851ef57afe6054560b4e6cae71
Animenosekai <40539549+Animenosekai@users.noreply.github.com>
2022-08-10 18:52:53 +0200

    yea

--- miko/parser/list.py
+++ miko/parser/list.py
@@ -114,7 +114,7 @@ class Parameter(ListElement):
             param = self.signature.parameters[self.name]
             annotation = param.annotation
             if annotation is not inspect._empty and str(annotation).strip().replace(" ", "") != "<class 'inspect._empty'>":
-                if hasattr(annotation, "__origin__") and annotation.__origin__ == typing.Union:
+                if hasattr(annotation, "__origin__") and annotation.__origin__ is typing.Union:
                     for a in annotation.__args__:
                         if str(a).lower().strip('"').strip("'").strip() not in {str(r).lower() for r in results}:
                             results.add(a)
